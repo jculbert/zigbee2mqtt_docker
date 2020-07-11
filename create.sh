f848943dc3066813bd216f4c9b50086b2a34a79b
Jeff <jeff.culbert@gmail.com>
2020-07-11 19:48:34 -0400

    Changed volume mapping to local directory

--- create.sh
+++ create.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-docker run -d --restart unless-stopped --device=/dev/ttyUSB0 -v /mnt/dockervm/zigbee2mqtt/data:/app/data --network host --name zigbee2mqtt koenkk/zigbee2mqtt:1.14.1
+docker run -d --restart unless-stopped --device=/dev/ttyUSB0 -v /root/builds/zigbee2mqtt/dockervm/zigbee2mqtt/data:/app/data --network host --name zigbee2mqtt koenkk/zigbee2mqtt:1.14.1
