571f70b64276f0fb113ecbe32b65eb6252270900
Jeff <culbair@gmail.com>
2025-09-13 14:53:06 -0400

    Changed backup.sh to new aes method

--- backup.sh
+++ backup.sh
@@ -4,6 +4,5 @@ import subprocess
 
 fn = 'zigbee2mqtt' + datetime.today().strftime('%Y-%m-%d') + '.tz'
 subprocess.call(["tar", "cvzf", fn, "dockervm/zigbee2mqtt/data"])
-#subprocess.call(["encrypt.sh", fn])
-#subprocess.call(["mv", fn + ".des", "/mnt/backup"])
-#subprocess.call(["rm", fn])
+subprocess.call(["encrypt_aes.sh", fn])
+subprocess.call(["rm", fn])
